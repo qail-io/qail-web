c46e2753e0d115ec6794f88a4a49e9489c6e5b86
verysignvl <qvilinx@gmail.com>
2026-01-01 00:31:24 +0800

    feat: add changelog, benchmark, and expressions pages with updated documentation and navigation.

--- public/docs/searcher-c2a407aa.js
+++ public/docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d95e44b2.js',
+                path_to_root + 'searchindex-10c225ba.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
