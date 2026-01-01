892feef6ba21c6c0d4e34d6708329cba66092232
verysignvl <qvilinx@gmail.com>
2026-01-02 02:05:20 +0800

    feat: Introduce qail-redis with a unified API and hybrid architecture, updating documentation and changelog.

--- public/docs/searcher-c2a407aa.js
+++ public/docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8b4a691a.js',
+                path_to_root + 'searchindex-dc583769.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
