bbc7729bae8db1f40efc50c1a5f666e351091931
verysignvl <qvilinx@gmail.com>
2026-01-12 11:14:22 +0800

    i

--- public/docs/searcher-c2a407aa.js
+++ public/docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f5d45903.js',
+                path_to_root + 'searchindex-daef3368.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
