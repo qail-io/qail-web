3eab4e9dc8ac9e01915a0ee50284c354b5cafed8
verysignvl <qvilinx@gmail.com>
2026-02-14 02:36:41 +0800

    feat: Add gateway and benchmarks documentation, revamp roadmap, and refresh website content and UI.

--- public/docs/searcher-c2a407aa.js
+++ public/docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c0409787.js',
+                path_to_root + 'searchindex-4a5273a6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
